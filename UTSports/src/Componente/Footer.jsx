e1627e102b43b2c7035713495d72f517958dcc01
TotoHacker <totochucl@gmail.com>
2023-07-01 23:54:13 -0500

    Añadimos un contenedor para los titulos del footer

--- UTSports/src/Componente/Footer.jsx
+++ UTSports/src/Componente/Footer.jsx
@@ -2,10 +2,14 @@ import React from "react";
 function Footer() {
     return (
         <footer>
-            <li>
-                <p className="textF">Redes</p>
-                <p className="textF">Tus Comentarios</p >
-            </li>
+        <li className="conten">
+         <p className="textF">Redes</p>
+         
+         <p className="textF">Tus Comentarios</p >
+        </li>
+        <li className="conten">
+            <p>© 2023 BC Toño</p>
+        </li>
         </footer>
         
     );
